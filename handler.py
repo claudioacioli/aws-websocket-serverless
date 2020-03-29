13c6bf9f550842bc18bc02deefdb255e08d3108c
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-29 20:20:50 -0300

    feat: Add function to connections handle

--- handler.py
+++ handler.py
@@ -7,14 +7,15 @@ import time
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
 
-table_name = os.getenv('MESSAGES_TABLE')
+messages_table = os.getenv("MESSAGES_TABLE")
+connections_table = os.getenv("CONNECTIONS_TABLE")
 dynamodb = boto3.resource("dynamodb")
 
 
 def ping(event, context):
     logger.info("Table name: " + table_name)
  
-    table = dynamodb.Table(table_name)
+    table = dynamodb.Table(messages_table)
     timestamp = int(time.time())
     table.put_item(Item={"Room": "general", "Index":0, 
         "Timestamp": timestamp, "Username": "ping-user",
@@ -28,5 +29,36 @@ def ping(event, context):
     }
 
     return response
+
+
+def connection_manager(event, context):
+
+    """
+    Handles connecting and disconnecting for the websocket.
+    """
+
+    connectionID = event["requestContext"].get("connectionId")
+
+    if event["requestContext"]["eventType"] == "CONNECT":
+        
+        logger.info("Connect requested")
+        
+        table = dynamodb.Table(connections_table)
+        table.put_item(Item={"ConnectionId": connectionID})
+
+        return {"statusCode": 200, "body": "Connect successful"}
+
+    elif event["requestContext"]["eventType"] == "DISCONNECT":
+        
+        logger.info("Disconnect requested")
+
+        table = dynamodb.Table(connections_table)
+        table.delete_item(key={"ConnectionId": connectionID})
+
+        return {"statusCode": 200, "body": "Disconnect successful"}
+
+    else:
+        logger.error("Connection manager received unrecognized eventType.")
+        return {"statusCode": 200, "body": "Unrecognized eventType."}
 
 
