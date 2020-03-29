3e34cb3cc66f34a73b42b077b661def58c64061c
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-29 14:33:17 -0300

    feat: Add logs

--- handler.py
+++ handler.py
@@ -1,7 +1,11 @@
 import json
+import logging
 
+logger = logging.getLogger("handler_logger")
+logger.setLevel(logging.DEBUG)
 
 def ping(event, context):
+    logger.info("Ping requested.")
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
