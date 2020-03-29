facf9ea1b2452adc937db7e0bb60174f1658049e
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-29 19:21:37 -0300

    refactor: Add item to table

--- handler.py
+++ handler.py
@@ -1,19 +1,30 @@
 import json
 import logging
+import os
+import boto3
+import time
 
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
 
+table_name = os.getenv('MESSAGES_TABLE')
+dynamodb = boto3.resource("dynamodb")
+
+
 def ping(event, context):
-    logger.info("Ping requested.")
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    logger.info("Table name: " + table_name)
+ 
+    table = dynamodb.Table(table_name)
+    timestamp = int(time.time())
+    table.put_item(Item={"Room": "general", "Index":0, 
+        "Timestamp": timestamp, "Username": "ping-user",
+        "Content": "PING!"})
+
+    logger.debug("Item added to the database.")
 
     response = {
         "statusCode": 200,
-        "body": "PONG"
+        "body": "PONG!"
     }
 
     return response
